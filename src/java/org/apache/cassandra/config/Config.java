9961a44e84a39ac19a93e0fe1d3088697f8cec42
Sam Tunnicliffe <samt@apache.org>
2023-03-01 10:03:44 +0000

    [CEP-21] Include current epoch in internode header
    Co-authored-by: Marcus Eriksson <marcuse@apache.org>
    Co-authored-by: Alex Petrov <oleksandr.petrov@gmail.com>
    Co-authored-by: Sam Tunnicliffe <samt@apache.org>

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -1252,5 +1252,6 @@ public class Config
 
     public double severity_during_decommission = 0;
 
-    public StorageCompatibilityMode storage_compatibility_mode = StorageCompatibilityMode.CASSANDRA_4;
+    // TODO Revisit MessagingService::current_version
+    public StorageCompatibilityMode storage_compatibility_mode = StorageCompatibilityMode.NONE;
 }
