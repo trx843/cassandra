f917477a3901c6b6bd8ef7df23fdcda5da3de558
Sam Tunnicliffe <samt@apache.org>
2023-03-06 10:57:04 +0000

    [CEP-21] Bulk update of unit and dtests
    Updates the existing unit and and dtests to work with TCM. In the vast
    majority of cases, this just means changes to initialization or to
    slightly updated method signatures.
    
    In CEP-21 generally, the intention has been not to modify existing
    public interfaces at all and to limit any changes to code on the
    boundaries of internal subsystems. In addition, care has been taken to
    only make minimal modifications to existing tests, and to preserve their
    invariants. So although this commit is fairly large in terms of
    number of files, it's changes should be semantically quite light.
    
    Co-authored-by: Marcus Eriksson <marcuse@apache.org>
    Co-authored-by: Alex Petrov <oleksandr.petrov@gmail.com>
    Co-authored-by: Sam Tunnicliffe <samt@apache.org>

--- test/unit/org/apache/cassandra/cql3/statements/DescribeStatementTest.java
+++ test/unit/org/apache/cassandra/cql3/statements/DescribeStatementTest.java
@@ -35,12 +35,11 @@ import org.apache.cassandra.cql3.CQLTester;
 import org.apache.cassandra.cql3.CqlBuilder;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.locator.InetAddressAndPort;
-import org.apache.cassandra.locator.TokenMetadata;
 import org.apache.cassandra.schema.CompactionParams;
 import org.apache.cassandra.schema.Schema;
 import org.apache.cassandra.schema.TableId;
 import org.apache.cassandra.schema.TableMetadata;
-import org.apache.cassandra.service.StorageService;
+import org.apache.cassandra.tcm.ClusterMetadata;
 import org.apache.cassandra.transport.ProtocolVersion;
 
 import static java.lang.String.format;
@@ -455,10 +454,9 @@ public class DescribeStatementTest extends CQLTester
                               "ByteOrderedPartitioner",
                               DatabaseDescriptor.getEndpointSnitch().getClass().getName()));
         }
-
-        TokenMetadata tokenMetadata = StorageService.instance.getTokenMetadata();
-        Token token = tokenMetadata.sortedTokens().get(0);
-        InetAddressAndPort addressAndPort = tokenMetadata.getAllEndpoints().iterator().next();
+        ClusterMetadata metadata = ClusterMetadata.current();
+        Token token = metadata.tokenMap.tokens().get(0);
+        InetAddressAndPort addressAndPort = metadata.directory.allAddresses().iterator().next();
 
         assertRowsNet(executeDescribeNet(KEYSPACE_PER_TEST, "DESCRIBE CLUSTER"),
                       row("Test Cluster",
