1e71de461d7ba87dec078b9f5a07cf80642ea495
Marcus Eriksson <marcus_eriksson@apple.com>
2023-08-07 18:49:30 +0200

    [CEP-21] Update index status fixes post-rebase

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2140,7 +2140,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         return value.value.split(VersionedValue.DELIMITER_STR, -1);
     }
 
-    private void updateIndexStatus(InetAddressAndPort endpoint, VersionedValue versionedValue)
+    public static void updateIndexStatus(InetAddressAndPort endpoint, VersionedValue versionedValue)
     {
         IndexStatusManager.instance.receivePeerIndexStatus(endpoint, versionedValue);
     }
